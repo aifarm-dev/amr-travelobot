523b2782d43f3b67df188fc1abbce5fbe6bd4e18
rithysourn <rithysourn@khamsagroup.com>
2022-08-04 13:19:29 +0700

    change and remove unecessay

---
+++ amr_launch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+source /amr-travelobot/ros_ws/install/setup.sh
+ros2 launch odrive_bringup odrive.launch.py
---
+++ ros_ws/amr_launch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+
+    source ./install/setup.sh
+ros2 launch odrive_bringup odrive.launch.py
